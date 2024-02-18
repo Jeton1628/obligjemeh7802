0462ad2aa78ee77b9d6917c0d163b8d711039678
Jeton1628 <jemeh7802@oslomet.no>
2024-02-18 21:41:38 +0100

    Initial commit

---
+++ src/main/java/com/example/obligjemeh7802/Obligjemeh7802Application.java
@@ -0,0 +1,13 @@
+package com.example.obligjemeh7802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obligjemeh7802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Obligjemeh7802Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obligjemeh7802/Obligjemeh7802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligjemeh7802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligjemeh7802ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
